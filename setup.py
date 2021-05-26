d86b1836f47e2b9b10ce080a5fd25f87e768e9a7
Trevor Lancon <37873827+tlancon@users.noreply.github.com>
2021-05-26 12:08:21 -0500

    Clearer description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name='CIM',
     version='0.0.1',
     author='Trevor Lancon',
-    description='Map images to one another with landmarks and view them in napari.',
+    description='Map images to one another with landmarks and create a script view them in napari.',
     long_description='Align a series of images to a single reference using landmarks,'
                      'then create a napari script to open them in the context of one another. ',
     packages=setuptools.find_packages(),
